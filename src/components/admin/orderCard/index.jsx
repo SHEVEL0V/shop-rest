d7c0c8e0f280e0d27fa0c20a86c0adc6c3c91ae9
Андрій Шевельов <ashevelevmti@gmail.com>
2023-02-22 21:48:56 +0200

    add

--- src/components/admin/orderCard/index.jsx
+++ src/components/admin/orderCard/index.jsx
@@ -8,14 +8,25 @@ import s from "./style.module.css";
 import { useUpdateOrderMutation } from "../../../services/fetch";
 
 export default function OrderCard({ data }) {
-  const { email, telephone } = data?.user;
+  const { user, status, orders } = data;
+  const { email, telephone } = user;
   const [updateOrder] = useUpdateOrderMutation();
 
   const handleClick = () =>
-    updateOrder({ id: data._id, body: { status: "RESOLVED" } });
+    updateOrder({ id: data._id, body: { status: "PENDING" } });
 
   return (
-    <Card className={s.container}>
+    <Card
+      className={s.container}
+      style={{
+        backgroundColor:
+          status === "PENDING"
+            ? "#3fb1d5"
+            : status === "RESOLVED"
+            ? "#e3ffcc"
+            : "#e1e6e2",
+      }}
+    >
       <div className={s.emailContainer}>
         <Typography sx={{ fontSize: 20 }} color="text.secondary">
           {email}
@@ -24,12 +35,12 @@ export default function OrderCard({ data }) {
           tel:{telephone}
         </Typography>
         <Typography sx={{ fontSize: 16 }} color="text.secondary">
-          status:{data.status}
+          status:{status}
         </Typography>
       </div>
 
       <div className={s.orderContainer}>
-        {data?.orders?.map((el) => (
+        {orders?.map((el) => (
           <div className={s.itemOrderContainer} key={el._id}>
             <div className={s.itemOrder} style={{ minWidth: "150px" }}>
               brand: <b>{el.brand}</b>
