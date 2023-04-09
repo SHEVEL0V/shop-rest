87c83f8479630396c59a9666a9607530a0e222a1
Андрій Шевельов <ashevelevmti@gmail.com>
2023-04-09 14:14:11 +0300

    fix

--- src/components/modal/index.jsx
+++ src/components/modal/index.jsx
@@ -7,12 +7,11 @@ import s from "./style.module.css";
 export default function ModalCustom({ children, open = false, onClick }) {
   return (
     <Modal
-      open={open}
+      open={false}
       onClose={onClick}
       sx={{
         paddingTop: 10,
         paddingInline: 2,
-        display: open ? "block" : "none",
       }}
       closeAfterTransition={true}
     >
