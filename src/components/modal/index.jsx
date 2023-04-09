190d16f0cd6dce19c7549232c02714cfc9dc120d
Андрій Шевельов <ashevelevmti@gmail.com>
2023-04-09 14:03:46 +0300

    fix

--- src/components/modal/index.jsx
+++ src/components/modal/index.jsx
@@ -12,6 +12,7 @@ export default function ModalCustom({ children, open = false, onClick }) {
       sx={{
         paddingTop: 10,
         paddingInline: 2,
+        display: "none",
       }}
       closeAfterTransition={true}
     >
